e70784388c52927c646af02d08e273ab8f5e6b08
timp672 <90533976+timp672@users.noreply.github.com>
2021-09-12 17:22:18 -0400

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,10 @@
-package org.example;
-import java.util.Scanner;
 /*
  *  UCF COP3330 Fall 2021 Assignment 1 Solution
  *  Copyright 2021 Tim Phung
  */
+package org.example;
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
